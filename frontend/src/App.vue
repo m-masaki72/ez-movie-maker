9ffb0dbda1449ebfb3f27bb6703e3474a514259b
masaki.mori <masaki.mori@gree.net>
2020-12-18 00:38:21 +0900

    add sample front pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-menu-item index="home" :route="{ name:'home' }">Home</el-menu-item>
+      <el-menu-item index="about" :route="{ name:'about' }">About</el-menu-item>
+      <el-menu-item>
+        <a href="https://element.eleme.io" target="_blank">Link</a>
+      </el-menu-item>
+    </el-menu>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      activeIndex: this.$route.name
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+a {
+  text-decoration: none;
 }
 </style>
